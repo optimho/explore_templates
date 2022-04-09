1d9ab72ff9e0dac51c811c14b7c7529422d16381
optimho <michael@duplessis.nz>
2022-04-10 08:57:28 +1200

    Add static content like a picture and css

--- help_app/views.py
+++ help_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+import random
+import time
+import logging
 
 # Create your views here.
 
 def help_app(request):
-    help_dict = {'example_item': " here is some help on the example item lorem"}
-    return render(request, 'help_app/index.html', context=help_dict)
+    help_dict = {'example_item': " here is some help on the example item lorem", "random_number": 0}
+    logging.basicConfig(filename='help.log', encoding='utf-8', level=logging.INFO)
+
+    while True:
+
+        n = random.randint(0, 100)
+        help_dict.update({"random_number": n})
+        logging.info(" This is the random {} number".format(help_dict.get("random_number")))
+        return render(request, 'help_app/index.html', context=help_dict)
+        time.sleep(10)
